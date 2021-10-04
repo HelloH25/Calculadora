77a9e4735cd8881a60f7481f59dd0b84a183dc1e
Michael Forero <mforeroroldan@gmail.com>
2021-10-04 15:57:40 -0500

    proximo a revisar

---
+++ JS/Calculadora.js
@@ -0,0 +1,50 @@
+// Variables de operacion
+let primerValor = '';
+let segundoValor = '';
+let operacion = '';
+
+// variables de resultado y limpieza
+let respuesta = document.getElementById("resultado");
+
+const digito = (d) => {
+    debugger;
+    respuesta.value +=  d;
+    primerValor = primerValor + d;
+    console.log(respuesta.value);
+    console.log(primerValor);
+};
+
+const op = (o) => {
+    operacion = o;
+    respuesta.value += o;
+    segundoValor = primerValor;
+    primerValor = '';
+};
+
+const resultado = () => {
+
+    switch (operacion) {
+        case '+':
+            respuesta.value = segundoValor + primerValor;
+            break;
+        case '-':
+            respuesta.value = segundoValor - primerValor;
+            break;
+        case '*':
+            respuesta.value = segundoValor * primerValor;
+            break;
+        case '/':
+            respuesta.value = segundoValor / primerValor;
+            break;
+        default:
+            break;
+    }
+};
+
+const limpiar = () => {
+    respuesta.value = '';
+    primerValor = '';
+    segundoValor = '';
+    operacion = '';
+}
+
